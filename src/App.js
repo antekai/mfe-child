cdbc6865ba846f1fea4adefd56db724b759f3832
AndreasT <andreas.teneketzoglou@medwing.com>
2021-09-14 19:10:30 +0200

    init micro-fe child

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
-import { useEffect, useState } from "react";
 import client from "./apollo";
 import ExchangeRates from "./ExchangeRates";
-import Navigation from "./Navigation";
 
 const App = () => {
-  const [active, setActive] = useState("");
-
-  useEffect(() => {
-    setActive(window.location.pathname);
-  }, []);
-
-  const routes = [
-    { label: "Parent", path: "/" },
-    { label: "Child", path: "child" },
-  ];
-
   return (
     <ApolloProvider client={client}>
-      <Navigation routes={routes} />
-      {active === "/" && <ExchangeRates />}
+      <ExchangeRates />
     </ApolloProvider>
   );
 };
